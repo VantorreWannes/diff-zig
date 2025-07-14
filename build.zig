c76793694032619459706b5ecae1f7f1e13c697b
Wannes Vantorre <vantorrewannes@gmail.com>
2025-07-14 09:34:57 +0200

    refactor: Correct cos_lcs imports.

--- build.zig
+++ build.zig
@@ -22,6 +22,9 @@ pub fn build(b: *std.Build) void {
     // target and optimize options) will be listed when running `zig build --help`
     // in this directory.
 
+    const cos_lcs_pkg = b.dependency("cos_lcs_zig", .{ .target = target, .optimize = optimize });
+    const cos_lcs_mod = cos_lcs_pkg.module("cos_lcs_zig");
+
     // This creates a module, which represents a collection of source files alongside
     // some compilation options, such as optimization mode and linked system libraries.
     // Zig modules are the preferred way of making Zig code available to consumers.
@@ -40,10 +43,10 @@ pub fn build(b: *std.Build) void {
         // Later on we'll use this module as the root module of a test executable
         // which requires us to specify a target.
         .target = target,
+        .imports = &.{
+            .{ .name = "cos_lcs", .module = cos_lcs_mod },
+        },
     });
-
-    const cos_lcs_pkg = b.dependency("cos_lcs_zig", .{ .target = target, .optimize = optimize });
-    const cos_lcs_mod = cos_lcs_pkg.module("cos_lcs_zig");
 
     // Here we define an executable. An executable needs to have a root module
     // which needs to expose a `main` function. While we could add a main function
